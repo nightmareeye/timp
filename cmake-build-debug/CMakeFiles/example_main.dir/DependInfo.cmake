8bd4f28cac0a8e63b5d821fd393d27e92cdd5372
foxmulder <foxmulder@gitwork.ru>
2021-09-27 15:21:31 +0300

    добавленo через UBUNTU

---
+++ cmake-build-debug/CMakeFiles/example_main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/Desktop/timp/example/example.cpp" "/home/user/Desktop/timp/cmake-build-debug/CMakeFiles/example_main.dir/example/example.cpp.o"
+  "/home/user/Desktop/timp/example/main.cpp" "/home/user/Desktop/timp/cmake-build-debug/CMakeFiles/example_main.dir/example/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
