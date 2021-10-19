665f5385bfeca40a42689c0b0c56be6eee48dc14
nightmareeye <mrrexcrafter@gmail.com>
2021-10-19 17:31:35 -0400

    Добавлен бесконечный цикл в main.cpp

--- pz03/main.cpp
+++ pz03/main.cpp
@@ -1,4 +1,23 @@
-//
-// Created by user on 10/19/21.
-//
+#include "task03.h"
+#include <iostream>
 
+using namespace task3;
+using namespace std;
+
+int main() {
+   while (true) {
+        int x;
+        cin >> x;
+        switch (x) {
+            case (1) :
+                continue;
+            case (2):
+                continue;
+            case (3) :
+                continue;
+            case (4):
+                return false;
+        }
+    }
+    return 0;
+}
