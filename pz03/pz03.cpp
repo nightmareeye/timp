460ad07f1111830d1eb23c868c5c9dd6e8000e00
nightmareeye <mrrexcrafter@gmail.com>
2021-10-19 17:56:45 -0400

    Добавлены функции в файл pz03.cpp

--- pz03/pz03.cpp
+++ pz03/pz03.cpp
@@ -1,4 +1,19 @@
-//
-// Created by user on 10/19/21.
-//
+#include "task03.h"
+#include <iostream>
 
+using namespace std;
+
+namespace task3 {
+    int copier(char *mas, char *mas2) {
+        int k = 0;
+        return k;
+    }
+    double summ(char* mas) {
+        double k = 0;
+        return k;
+    }
+    int crypto(char* mas, char* mas2, char *mas3) {
+        int k = 0;
+        return k;
+    }
+}
