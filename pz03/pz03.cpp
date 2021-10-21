5d46ad3a8647dbea953246a609c73d9e889568b4
nightmareeye <mrrexcrafter@gmail.com>
2021-10-21 19:12:59 -0400

    Added comments to errors

--- pz03/pz03.cpp
+++ pz03/pz03.cpp
@@ -14,70 +14,98 @@ namespace task3 {
         int k = 0;                      //Выводное знаение: кол-во скопированных строк
         string line;                    //Пустая строка, считывающая строчки входного файла
         ifstream fin(mas);
-
-        ofstream fout(mas2, ios::app);
-        while (fin) {
-            getline(fin, line);
-            fout << line << '\n';
-            k++;
+        char ch;
+        if(!fin){
+            return -1;                  //-2, если не существует
         }
-        fin.close();
-        fout.close();
-        return k;
+        else {
+            if(fin.peek()==EOF){
+                return 0;               //-1, если пустой
+            }
+            ofstream fout(mas2, ios::app);
+            while (fin) {
+                getline(fin, line);
+                fout << line << '\n';
+                k++;
+            }
+            fin.close();
+            fout.close();
+            return k;
+        }
     }
     double summ(char* mas) {
         double k = 0;                   //Выводное значение: сумма чисел
         string line = "";
         fstream temp;
-        string st = "";                 //Строка 1
-        string s = "";                  //Строка 2
+        char ch;
         temp.open(mas, fstream::in);
-        while (getline(temp, line)) {   //Помещение всех чисел файла построчно в строку 1
-            replace(line.begin(), line.end(), ',', '.');
-            st += line;
-            st += " ";
+        if(!temp){
+            return -2;                  //-2, если не существует
         }
-        temp.close();
-        temp.open(mas, fstream::out | fstream::trunc);
-        temp << st;                     //Перезаписывание файла: строка 1
-        temp.close();
-        temp.open(mas, fstream::in);
-        while (getline(temp, line)) {   //Помещение всех чисел файла построчно с переносом строки в строку 2
-            replace(line.begin(), line.end(), ' ', '\n');
-            s += line;
-        }
-        temp.close();
-        temp.open(mas, fstream::out | fstream::trunc);
-        temp << s;                      //Перезаписывание файла: строка 2
-        temp.close();
-        temp.open(mas, fstream::in);
-        while (getline(temp, line)) {   //Суммирование чисел
-            double d = strtod(line.c_str(), NULL);
+        else {
+            if (temp.peek() == EOF) {
+                return -1;              //-1, если пустой
+            }
+            temp.close();
+            string st = "";                 //Строка 1
+            string s = "";                  //Строка 2
+            temp.open(mas, fstream::in);
+            while (getline(temp, line)) {   //Помещение всех чисел файла построчно в строку 1
+                replace(line.begin(), line.end(), ',', '.');
+                st += line;
+                st += " ";
+            }
+            temp.close();
+            temp.open(mas, fstream::out | fstream::trunc);
+            temp << st;                     //Перезаписывание файла: строка 1
+            temp.close();
+            temp.open(mas, fstream::in);
+            while (getline(temp, line)) {   //Помещение всех чисел файла построчно с переносом строки в строку 2
+                replace(line.begin(), line.end(), ' ', '\n');
+                s += line;
+            }
+            temp.close();
+            temp.open(mas, fstream::out | fstream::trunc);
+            temp << s;                      //Перезаписывание файла: строка 2
+            temp.close();
+            temp.open(mas, fstream::in);
+            while (getline(temp, line)) {   //Суммирование чисел
+                double d = strtod(line.c_str(), NULL);
 
-            k += d;
+                k += d;
+            }
+            temp.close();
+            return k;
         }
-        temp.close();
-        return k;
     }
     int crypto(char* mas, char* mas2, char *mas3) {
         int k = 0;                      //Выводное значение: число байт
         ifstream fin(mas);
-        ofstream fout(mas2, ios::trunc);
-        string line;
-        while (getline(fin, line)) {
-            int f = 0;
-            for (int i = 0; i < line.length(); i++) {
-                if (f > strlen(mas3)) f = 0;
-                char l;
-                l = line[i] ^ mas3[f];  //Шифрование XOR
-                f++;
-                fout << l;
-                k++;
-            }
-            fout << '\n';
+        char ch;
+        if(!fin){
+            return -2;                  //-2, если не существует
         }
-        fin.close();
-        fout.close();
-        return k;
+        else {
+            if (fin.peek() == EOF) {
+                return -1;              //-1, если пустой
+            }
+            ofstream fout(mas2, ios::trunc);
+            string line;
+            while (getline(fin, line)) {
+                int f = 0;
+                for (int i = 0; i < line.length(); i++) {
+                    if (f > strlen(mas3)) f = 0;
+                    char l;
+                    l = line[i] ^ mas3[f];  //Шифрование XOR
+                    f++;
+                    fout << l;
+                    k++;
+                }
+                fout << '\n';
+            }
+            fin.close();
+            fout.close();
+            return k;
+        }
     }
 }
