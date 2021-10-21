2ecf838b20767e2d681762dc61b8e3366c9af472
nightmareeye <mrrexcrafter@gmail.com>
2021-10-21 16:32:52 -0400

    Добавлены изменения в CMakeLists.txt , а также необходимые файлы и каталог pz05

---
+++ pz05/pz05.cpp
@@ -0,0 +1,4 @@
+//
+// Created by user on 10/21/21.
+//
+
---
+++ pz05/pz05.h
@@ -0,0 +1,8 @@
+//
+// Created by user on 10/21/21.
+//
+
+#ifndef TEST_EMPTY_PZ05_H
+#define TEST_EMPTY_PZ05_H
+
+#endif //TEST_EMPTY_PZ05_H
