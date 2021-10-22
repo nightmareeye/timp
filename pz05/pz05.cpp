59d9b882c2b1b78da58be032275bd52f5c648e83
nightmareeye <mrrexcrafter@gmail.com>
2021-10-21 21:04:24 -0400

    Created 3rd test for task3::copier (input file exist & not empty)

--- pz05/pz05.cpp
+++ pz05/pz05.cpp
@@ -17,3 +17,10 @@ TEST_CASE("test2", "[und]")
     int t2 = task3::copier((char*)"../pz03/data/pz05/eksdi.txt",(char*)"../pz03/data/pz05/test1.3.txt");
     REQUIRE(t2==-1);
 }
+
+TEST_CASE("test3", "[true]")
+{
+    int t3= task3::copier((char*)"../pz03/data/pz05/test1.4.txt",(char*)"../pz03/data/pz05/test1.5.txt");
+    REQUIRE(t3==4);
+}
+
