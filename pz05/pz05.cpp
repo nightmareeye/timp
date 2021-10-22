b19cfb2708e66dc016d6655e9d12923660ba4247
nightmareeye <mrrexcrafter@gmail.com>
2021-10-21 21:10:31 -0400

    Created 4th test for task3::summ (input file is empty)

--- pz05/pz05.cpp
+++ pz05/pz05.cpp
@@ -9,13 +9,13 @@ TEST_CASE("test1","[false]")
 {
 
     int t1 = task3::copier((char*)"../pz03/data/pz05/test1.1.txt",(char*)"../pz03/data/pz05/test1.2.txt");
-    REQUIRE(t1==0);
+    REQUIRE(t1==-1);
 }
 
 TEST_CASE("test2", "[und]")
 {
     int t2 = task3::copier((char*)"../pz03/data/pz05/eksdi.txt",(char*)"../pz03/data/pz05/test1.3.txt");
-    REQUIRE(t2==-1);
+    REQUIRE(t2==-2);
 }
 
 TEST_CASE("test3", "[true]")
@@ -24,3 +24,8 @@ TEST_CASE("test3", "[true]")
     REQUIRE(t3==4);
 }
 
+TEST_CASE("test4", "[false]")
+{
+    int t4 = task3::summ((char*)"../pz03/data/pz05/test2.1.txt");
+    REQUIRE(t4==-1);
+}
