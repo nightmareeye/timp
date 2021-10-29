78e08e63ea1232b91c91c36b51cf88df38272bf8
foxmulder <foxmulder@gitwork.ru>
2021-10-29 16:56:18 +0300

    Added some more code to pz06/main.cpp

--- pz06/main.cpp
+++ pz06/main.cpp
@@ -1,14 +1,21 @@
+#include "iostream"
 #include <SFML/Graphics.hpp>
 
 int main()
-{   int countKeyPressed = 0;
+{
+    int countKeyPressed = 0;
     int countMouseClick = 0;
     std::string keyEnteredMessage("Key Entered:");
     std::string mouseClickMessage("Mouse Click:");
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
     sf::Font font;
+
     if(!font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-B.ttf")){
-    return 0;
-}
+        return 0;
+    }
+
     sf::Text textMouse("This is Mouse",font,20);
     textMouse.setColor(sf::Color::Green);
     textMouse.setStyle(sf::Text::Bold);
@@ -18,29 +25,26 @@ int main()
     textKey.setStyle(sf::Text::Italic);
     textKey.setPosition(0,50);
 
-    sf::RenderWindow window(sf::VideoMode(200, 200), "foxmulder");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-             switch(event.type) {
-                 case sf::Event::Closed: {
-                     window.close();
-                     break;
-                 }
-                 case sf::Event::MouseButtonReleased: {
-                     countMouseClick++;
-                     textMouse.setString(mouseClickMessage + std::to_string(countMouseClick));
-                     break;
-                 }
-                 case sf::Event::TextEntered: {
-                     countKeyPressed++;
-                     textKey.setString(keyEnteredMessage + std::to_string(countKeyPressed));
-                 }
-             }
+            switch(event.type){
+                case sf::Event::Closed:{
+                    window.close();
+                    break;
+                }
+                case sf::Event::MouseButtonReleased:{
+                    countMouseClick++;
+                    textMouse.setString(mouseClickMessage + std::to_string(countMouseClick));
+                    break;
+                }
+                case sf::Event::TextEntered:{
+                    countKeyPressed++;
+                    textKey.setString(keyEnteredMessage + std::to_string(countKeyPressed));
+                }
+            }
         }
         window.clear();
         window.draw(textMouse);
