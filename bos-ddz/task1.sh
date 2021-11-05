cc679f0b64847f8e2c8525c3e5d53c004354dd4e
foxmulder <foxmulder@gitwork.ru>
2021-11-05 13:49:46 +0300

    Fixed bugs in bos-ddz

--- bos-ddz/task1.sh
+++ bos-ddz/task1.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-read d2;
-read d1;
-read fn;
-N1=18; N3=1;
+N1=$(echo $3 | head --bytes=9 | tail --bytes=2)
+xx=$(echo $3 | head --bytes=6 | tail --bytes=1)
+N3=$(($xx-2))
 for N2 in 2 3 4 5
 do 
-var1=`awk 'END{print NR}' ./$d1/$N2.txt`
-var2=$(((((($N1+$N2*$N3))%$var1))+1)) 
-string=`awk 'NR=='$var2' {print; exit}' ./$d1/$N2.txt`
-cd ./$d2
-touch task$N2-$var2.sh
-echo "#!/bin/bash" >> task$N2-$var2.sh
-echo $fn >> task$N2-$var2.sh
-echo "$string" >> task$N2-$var2.sh
-cd ..
+	var1=`awk 'END{print NR}' ./$2/$N2.txt`
+	var2=$(((((($N1+$N2*$N3))%$var1))+1)) 
+	string=`awk 'NR=='$var2' {print; exit}' ./$2/$N2.txt`
+	cd ./$1
+	touch task$N2-$var2.sh
+	echo "#!/bin/bash" >> task$N2-$var2.sh
+	echo "#$3" >> task$N2-$var2.sh
+	echo "#$string" >> task$N2-$var2.sh
+	cd ..
 done
 echo "done"
