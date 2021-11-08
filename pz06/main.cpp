399ce07ebb15b901b0027e1d5d3d68510573b385
nightmareeye <mrrexcrafter@gmail.com>
2021-11-08 17:05:16 -0500

    Added sf::text to an external func

--- pz06/main.cpp
+++ pz06/main.cpp
@@ -5,6 +5,17 @@
 #include <ctime>
 
 int NewThreadFunc(sf::RenderWindow & window){
+    window.setActive();
+    sf::Font font;
+    if(!font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-Th.ttf")){
+        return 0;
+    }
+    font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-B.ttf");
+    sf::Text text("",font,20);
+    text.setColor(sf::Color::Cyan);
+    text.setPosition(10,10);
+
+
     return 0;
 }
 
