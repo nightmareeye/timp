c21520692ebd9314bb1b5cce3d71c5032020054e
nightmareeye <mrrexcrafter@gmail.com>
2021-11-08 16:59:51 -0500

    Added thread function, added a renderwindow to int main()

--- pz06/main.cpp
+++ pz06/main.cpp
@@ -4,6 +4,14 @@
 #include "string"
 #include <ctime>
 
+int NewThreadFunc(sf::RenderWindow & window){
+    return 0;
+}
+
 int main(){
+    sf::RenderWindow window(sf::VideoMode(800, 600), "main window");
+    sf::Thread mainThread(NewThreadFunc, std::ref(window));
+    mainThread.launch();
+    mainThread.wait();
     return 0;
 }
